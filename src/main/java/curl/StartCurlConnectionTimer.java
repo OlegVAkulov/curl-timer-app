ce329a42f2d8762a195cb8a3ee593203e46c6058
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-23 17:49:20 +0300

    date - 1

--- src/main/java/curl/StartCurlConnectionTimer.java
+++ src/main/java/curl/StartCurlConnectionTimer.java
@@ -27,7 +27,7 @@ private String info ;
 
         Calendar startDataTime = Calendar.getInstance();
         Date date = startDataTime.getTime();
-        startDataTime.set(2022, date.getMonth(), date.getDay(), 17, 29);
+        startDataTime.set(2022, date.getMonth(), 24, 17, 29);
         date = startDataTime.getTime();
         TimerTask timerTask = new StartCurlConnectionTimer();
         this.info = "we are the champions!";
